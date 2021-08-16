d42aa10e110e6a5f7fe2929293b80f85939b6073
Ash_360 <abbasiashin0@gmail.com>
2021-08-17 00:10:15 +0530

    Completed Mark Twelve App

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fun with Triangles
+
+A ReactJS App to do the following:
+
+- Calculate Area of a Triangle.
+- To Find the Hypotenuse of a right Triangle.
+- To find third angle if two angles are given.
+- Check wheather a triangle is obtuse, acute or right.
+- Check wheather a triangle is scalene, equilateral or isoceles.
+- Calculate sum of angles forms a triangles
+
+Note: Basically it's a questionaire application that expects correct answer from the users.
+
+> This app is part of assigment for NeoG Camp Admission.
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import CheckByAngles from "./check-by-angles";
+import TypeOfTriangle from "./type-of-triangle";
+import GuessThirdAngle from "./guess-third-angle";
+import Hypotenuse from "./hypotenuse";
+import Area from "./area";
+import Header from "./header";
+import { useState } from "react";
+import SumOfAngles from "./sum-of-angles";
+
+function App() {
+  const [route, setRoute] = useState("area");
+  let element = <Area />;
+  if (route === "area") {
+    element = <Area />;
+  } else if (route === "hypotenuse") {
+    element = <Hypotenuse />;
+  } else if (route === "third") {
+    element = <GuessThirdAngle />;
+  } else if (route === "sum") {
+    element = <SumOfAngles />;
+  } else if (route === "angle") {
+    element = <TypeOfTriangle />;
+  } else if (route === "side") {
+    element = <CheckByAngles />;
+  } else {
+    element = <Area />;
+  }
+  return (
+    <>
+      <Header setRoute={setRoute} />
+      <div className="container">
+        <main className="main">{element}</main>
+      </div>
+    </>
+  );
+}
+
+export default App;
